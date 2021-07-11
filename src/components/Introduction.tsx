c4e5d33cc1c05102890bf38b77abd8828d063957
MK de Silva <dev.mkdesilva@gmail.com>
2021-07-11 10:44:44 +0700

    Changed styles to suit mobile

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -4,52 +4,61 @@ import strings from '../strings';
 import Video from './Video';
 
 const useStyles = makeStyles({
-  introduction: {
-    paddingTop: 40,
-    flexDirection: 'column',
-  },
-  title: {},
-  videoSection: {
-    display: 'flex',
-    paddingTop: 40,
-    flexDirection: 'row',
-  },
-  propaganda: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
-  },
+    introduction: {
+        paddingTop: 40,
+        flexDirection: 'column',
+    },
+    title: {},
+    videoSection: {
+        display: 'flex',
+        paddingTop: 40,
+        flexDirection: 'column',
+    },
+    propaganda: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+    },
+    videoBox: {
+        position: 'relative',
+        paddingBottom: '56.25%',
+    },
+    propagandaSection: {
+        paddingTop: 16,
+    },
 });
 
 const Introduction: React.FC = () => {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <Container className={classes.introduction}>
-      <Box className={classes.title}>
-        <Typography variant="h4" align="center">
-          {strings.welcome}
-        </Typography>
-      </Box>
-      <Box className={classes.videoSection}>
-        <Box>
-          <Video />
-          <Typography style={{ paddingTop: 8 }} variant="body2" align="center">
-            {strings.watchFirst}
-          </Typography>
-        </Box>
-        <Container className={classes.propaganda}>
-          <Typography variant="body1" align="center">
-            {strings.propagandaPart1}
-          </Typography>
-          <Typography style={{ paddingTop: 20 }} variant="body1" align="center">
-            {strings.propagandaPart2}
-          </Typography>
+    return (
+        <Container className={classes.introduction}>
+            <Box className={classes.title}>
+                <Typography variant="h4" align="center">
+                    {strings.welcome}
+                </Typography>
+            </Box>
+            <Box className={classes.videoSection}>
+                <Box className={classes.videoBox}>
+                    <Video />
+                </Box>
+                <Typography style={{ paddingTop: 16 }} variant="body2" align="center">
+                    {strings.watchFirst}
+                </Typography>
+            </Box>
+            <Box className={classes.propagandaSection}>
+                <Container className={classes.propaganda}>
+                    <Typography variant="body1" align="center">
+                        {strings.propagandaPart1}
+                    </Typography>
+                    <Typography style={{ paddingTop: 20 }} variant="body1" align="center">
+                        {strings.propagandaPart2}
+                    </Typography>
+                </Container>
+            </Box>
         </Container>
-      </Box>
-    </Container>
-  );
+    );
 };
 
 export default Introduction;
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
 const Video: React.FC = () => {
-  return (
-    <iframe
-      style={{ borderWidth: 0 }}
-      src="https://player.vimeo.com/video/569812014"
-      width="640"
-      height="360"
-      allow="autoplay; fullscreen; picture-in-picture"
-      allowFullScreen
-    ></iframe>
-  );
+    return (
+        <iframe
+            style={{
+                borderWidth: 0,
+                width: '100%',
+                height: '100%',
+                position: 'absolute',
+            }}
+            src="https://player.vimeo.com/video/571266893"
+            allow="autoplay; fullscreen; picture-in-picture"
+            allowFullScreen
+        />
+    );
 };
 
 export default Video;
