58e43883107a11fd10464a3537d67e35d9af412a
ChenukaThishen <86372459+ChenukaThishen@users.noreply.github.com>
2021-07-13 11:49:26 +0530

    PieChart

--- src/strings.ts
+++ src/strings.ts
@@ -1,3 +1,4 @@
+
 const strings = {
     welcome: 'WELCOME TO YOUR 5 MINUTE SELF-ASSESSMENT',
     appTitle: 'Burnout Coach',
@@ -8,6 +9,8 @@ const strings = {
         'Please know that only I have access to this information so the more honest and raw you can be, the better conversation we can have should you choose to book a Progression Session with me.',
     formTitle: 'Fill out all the segments, and press Show my results at the end to create your Wheel of Balance',
     submit: 'Show my results!',
+    textPiechart: 'Nice one! Here is your transformation blueprint in black and white. You\'ve been honest and outlined what\'s currently not working and what\'s in the gap to fix it. If you\'re like most of my clients, you know what you need to do, but you don\'t know where to start. That\'s where I come in. Apply for your FREE Progression Session for us to deep dive into your results, and during the call, if it\'s a fit, I\'ll be extending an invitation for us to work together. I only have limited space for free calls on my calendar so fill out your application now. I can\'t wait to meet you!',
+    
     love: {
         emoji: '❤️',
         title: 'Love & Romance',
