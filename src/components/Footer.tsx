47176cefd83080b35852c2a2cace70fbcb5fc5c4
MK de Silva <dev.mkdesilva@gmail.com>
2021-07-14 20:17:21 +0700

    Added book session on Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,30 +1,80 @@
 import React from 'react';
-import { Container, makeStyles } from '@material-ui/core';
+import { Button, Container, makeStyles, useMediaQuery, useTheme } from '@material-ui/core';
 import emilyFooter from '../images/emily-footer.png';
+import strings from '../strings';
 
 const useStyles = makeStyles({
-  footer: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  image: {
-    display: 'block',
-    width: '100vw',
-    height: 'auto',
-    maxHeight: '100%',
-    maxWidth: 'auto',
-  },
+    footer: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'relative',
+        flexDirection: 'column',
+    },
+    image: {
+        display: 'block',
+        width: '100vw',
+        height: 'auto',
+        maxHeight: '100%',
+        maxWidth: 'auto',
+        zIndex: 0,
+    },
+    smButtonContainer: {
+        position: 'absolute',
+        bottom: 5,
+        width: '100%',
+        zIndex: 5,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    bigButtonContainer: {
+        position: 'absolute',
+        bottom: 30,
+        width: '100%',
+        zIndex: 5,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    bigButton: {
+        backgroundColor: '#f78f28',
+        whiteSpace: 'nowrap',
+        color: '#303030',
+        fontSize: 14,
+    },
+    smButton: {
+        backgroundColor: '#f78f28',
+        whiteSpace: 'nowrap',
+        color: '#303030',
+        fontSize: 8,
+    },
 });
 
 const Footer: React.FC = () => {
-  const classes = useStyles();
+    const classes = useStyles();
+    const theme = useTheme();
+    const bigScreen = useMediaQuery(theme.breakpoints.up('sm'));
 
-  return (
-    <Container className={classes.footer}>
-      <img src={emilyFooter} className={classes.image} />
-    </Container>
-  );
+    const onBookSessionPressed = () => {
+        window.location.href = 'https://emilyclairecoaching.activehosted.com/f/1';
+    };
+
+    return (
+        <Container className={classes.footer}>
+            {`Small Screen: ${bigScreen}`}
+            <img src={emilyFooter} className={classes.image} />
+            <Container className={bigScreen ? classes.bigButtonContainer : classes.smButtonContainer}>
+                <Button
+                    className={bigScreen ? classes.bigButton : classes.smButton}
+                    onClick={onBookSessionPressed}
+                    variant="contained"
+                >
+                    {strings.bookSession}
+                </Button>
+            </Container>
+        </Container>
+    );
 };
 
 export default Footer;
